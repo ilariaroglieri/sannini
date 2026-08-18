2c063e7ca916e087b04d72e8bd3aba6abc0ea5eb
Ilaria Roglieri <ilariaroglieri@gmail.com>
2026-08-18 14:23:07 +0200

    homepage

--- site/snippets/blocks/special-block.php
+++ site/snippets/blocks/special-block.php
@@ -4,12 +4,25 @@
 
 <section class="module special-img-module">
   <div class="d-flex wrap m-column">
-    <?php foreach ($images as $img): ?>
+    <?php foreach ($images->slice(0, 3) as $img): ?>
       <div class="element reveal d-one-third" data-span-x="1">
-        <?= snippet('image-w-caption', [
-          'img' => $img,
-          'showCaption' => false
-        ]) ?>
+        <?= snippet('image-w-caption', ['img' => $img, 'showCaption' => false]) ?>
+      </div>
+    <?php endforeach ?>
+
+    <div class="caption-row d-whole d-flex flex-row v-center">
+      <?php foreach ($images as $img): ?>
+      <div class="caption element reveal d-1-twelfth">
+        <?php if ($img->caption()->isNotEmpty()): ?>
+          <figcaption class="s-xsmall"><?= html($img->caption()) ?></figcaption>
+        <?php endif ?>
+      </div>
+      <?php endforeach ?>
+    </div>
+
+    <?php foreach ($images->slice(3, 6) as $img): ?>
+      <div class="element reveal d-one-third spacing-b-2" data-span-x="1">
+        <?= snippet('image-w-caption', ['img' => $img, 'showCaption' => false]) ?>
       </div>
     <?php endforeach ?>
   </div>
